273df88d9b3af9d31d772ac2a79c61156d32b602
Lingzhi Deng <lingzhi.deng@mongodb.com>
2019-06-22 15:35:37 -0400

    use insertRecords interface

--- src/mongo/db/catalog/collection_impl.cpp
+++ src/mongo/db/catalog/collection_impl.cpp
@@ -354,9 +354,8 @@ StatusWithMatchExpression CollectionImpl::parseValidator(
 }
 
 Status CollectionImpl::insertDocumentsForOplog(OperationContext* opCtx,
-                                               const DocWriter* const* docs,
-                                               Timestamp* timestamps,
-                                               size_t nDocs) {
+                                               std::vector<Record>* records,
+                                               const std::vector<Timestamp>& timestamps) {
     dassert(opCtx->lockState()->isWriteLocked());
 
     // Since this is only for the OpLog, we can assume these for simplicity.
@@ -365,7 +364,7 @@ Status CollectionImpl::insertDocumentsForOplog(OperationContext* opCtx,
     invariant(!_validator);
     invariant(!_indexCatalog->haveAnyIndexes());
 
-    Status status = _recordStore->insertRecordsWithDocWriter(opCtx, docs, timestamps, nDocs);
+    Status status = _recordStore->insertRecords(opCtx, records, timestamps);
     if (!status.isOK())
         return status;
 
